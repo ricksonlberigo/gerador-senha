daf857afcca13b6252c3b10015d790a6d1f7c092
ricksonlberigo <ricksonl.berigo@outlook.com>
2023-07-13 11:06:02 -0300

    Adicionando o range

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Input de range sempre irá iniciar com um tamanho de 16
+let passwordLength = 16
+
 function generatePassword() {
   // Criando conjunto de caracteres que serão utilizados na criação da senha
   const chars =
@@ -7,7 +10,7 @@ function generatePassword() {
   let password = ''
 
   // Esse loop vai percorrer a quantidade de caracteres para a criação de senha, e gerar uma senha com o tamanho de 8 caracteres
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     // Eu transformo o número e gero um número aleatório vezes a quantidade de caracteres
     const randomNumber = Math.floor(Math.random() * chars.length)
     // Atribuo a senha vazia com o conjunto de caracteres recortados pela função substring, com o inicio do randomNumber, e o final dele
@@ -19,5 +22,12 @@ function generatePassword() {
   // Atribuo a senha gerada ao atributo de value do input no HTML
   inputEl.value = password
 }
+
+// Recuperando o input de range e adicionando um evento onde toda vez que for arrastado o range, o tamanho do password irá aumentar de acordo com o valor do input range e é criado uma nova senha ao fazer isso
+const passwordLengthEl = document.getElementById('password-length')
+passwordLengthEl.addEventListener('input', () => {
+  passwordLength = passwordLengthEl.value
+  generatePassword()
+})
 
 generatePassword()
